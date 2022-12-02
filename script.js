a92e13d47edce5d03aa00f22b552449c5ac9eea8
CarlosPrieto <carlosprieto66@icloud.com>
2022-12-02 13:46:45 -0800

    logic that creates a new book and adds to library array

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 'use strict'
 
+
+const myLibrary = [];
+
+//Constructor function for creating a new Book
+function Book(title, author, numPages, readBook) {
+    this.title = title,
+    this.author = author,
+    this.numPages = numPages,
+    this.readBook = readBook
+};
+
+const redHood = new Book('Red riding Hood', 'Grimm', 3, true);
+
+
+//Function that adds the new Book to myLibrary array
+const addBook = (newBook) => {
+    myLibrary.push(newBook);
+};
+
+console.log(addBook(redHood));
+
+console.log(myLibrary);
