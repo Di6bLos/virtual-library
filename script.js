fde2b9c23c699dc3da8fb41da2c0e74361de75d6
CarlosPrieto <carlosprieto66@icloud.com>
2023-01-15 15:58:51 -0800

    Installed eslint

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
-'use strict'
-//DOM elements
+/* eslint-disable no-unused-expressions */
 
-
-
-
-
-
-
-
+// DOM elements
 
 const myLibrary = [];
 
-//Constructor function for creating a new Book
+// Constructor function for creating a new Book
 function Book(title, author, numPages, readBook) {
-    this.title = title,
-    this.author = author,
-    this.numPages = numPages,
-    this.readBook = readBook
-};
+  this.title = title,
+  this.author = author,
+  this.numPages = numPages,
+  this.readBook = readBook;
+}
 
 const redHood = new Book('Red riding Hood', 'Grimm', 3, true);
 
-
-//Function that adds the new Book to myLibrary array
+// Function that adds the new Book to myLibrary array
 const addBook = (newBook) => {
-    myLibrary.push(newBook);
+  myLibrary.push(newBook);
 };
 
 console.log(addBook(redHood));
